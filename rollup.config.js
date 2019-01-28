264a2bd35e298fd2e098c8eccec0507af32bd4a7
Berkeleybross <berkeleybross@gmail.com>
2019-01-28 19:57:39 +0000

    Blank project with tests, linting, and compilation

---
+++ rollup.config.js
@@ -0,0 +1,35 @@
+import resolve from 'rollup-plugin-node-resolve'
+import commonjs from 'rollup-plugin-commonjs'
+import sourceMaps from 'rollup-plugin-sourcemaps'
+
+const pkg = require('./package.json')
+
+export default {
+  input: 'src/index.js',
+  output: [
+    {
+      file: pkg.main,
+      name: pkg.name,
+      format: 'umd',
+      sourcemap: true,
+      globals: {}
+    },
+    { file: pkg.module, format: 'es', sourcemap: true }
+  ],
+  // Indicate here external modules you don't wanna include in your bundle (i.e.: 'lodash')
+  external: [],
+  watch: {
+    include: 'src/**'
+  },
+  plugins: [
+    // Allow bundling cjs modules (unlike webpack, rollup doesn't understand cjs)
+    commonjs(),
+    // Allow node_modules resolution, so you can use 'external' to control
+    // which external modules to include in the bundle
+    // https://github.com/rollup/rollup-plugin-node-resolve#usage
+    resolve(),
+
+    // Resolve source maps to the original source
+    sourceMaps()
+  ]
+}
